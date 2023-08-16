4ab4b5494c896d452e33a996d72c5bb697f112e0
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-08-17 04:23:45 +0530

    Updated version

---
+++ src/com/lbrce/MyCube/CSA.java
@@ -0,0 +1,10 @@
+package com.lbrce.MyCube;
+
+public class CSA {
+
+	public void show(int i) {
+		System.out.println(4*i*i);
+		
+	}
+
+}
---
+++ src/com/lbrce/MyCube/TSA.java
@@ -0,0 +1,10 @@
+package com.lbrce.MyCube;
+
+public class TSA {
+
+	public void fun(int i) {
+		System.out.println(6*i*i);
+		
+	}
+
+}
