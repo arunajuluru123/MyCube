a7564df4f4b1b88de5563ed89403980479772183
AdmiN <AdmiN@asdf>
2023-08-17 09:24:08 +0530

    first version

---
+++ src/Cube.java
@@ -0,0 +1,11 @@
+public class Cube {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         CSA a=new CSA();
+         TSA t=new TSA();
+         Volume v=new Volume();
+	
+
+}
+}
